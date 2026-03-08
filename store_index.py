e70ec3f9072d81b380fc4d41e19c59d875cf64f1
MayuraThayalan <thayalanmayura@gmail.com>
2026-03-08 10:29:12 +0530

    implemented hybrid search

--- store_index.py
+++ store_index.py
@@ -1,42 +1,42 @@
 from dotenv import load_dotenv
 import os
 from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
-from pinecone import ServerlessSpec
+from pinecone import Pinecone, ServerlessSpec
+from pinecone_text.sparse import BM25Encoder
+from langchain_community.retrievers import PineconeHybridSearchRetriever
 from src.helper import load_pdf_files, filter_metadata, text_splitter, download_embeddings
 
 load_dotenv()       
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["GROQ_API_KEY"] = GROQ_API_KEY
-
 extracted_data = load_pdf_files(data="data/")
 filter_data = filter_metadata(extracted_data)
 text_chunks = text_splitter(filter_data)
-
 embeddings = download_embeddings()
 
-pinecone_api_key = PINECONE_API_KEY
-pc = Pinecone(api_key=pinecone_api_key)
-
+#Initializing the Database
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index_name = "medical-public-health-chatbot"
 
 if not pc.has_index(index_name):
     pc.create_index(
         name = index_name,
         dimension= 384, #dimension of the embeddings
-        metric= "cosine", #cosine similarity
+        metric= "dotproduct", 
         spec = ServerlessSpec(cloud="aws",region="us-east-1")
     )
 
 index = pc.Index(index_name)
 
-docsearch = PineconeVectorStore.from_documents(
-    documents=text_chunks,
-    index_name = index_name,
-    embedding= embeddings,
+#Training the keyword search
+bm25_encoder = BM25Encoder().default()
+bm25_encoder.fit([d.page_content for d in text_chunks])
+bm25_encoder.dump("bm25_values.json")
+
+#Creating the hybrid retriver
+retriever = PineconeHybridSearchRetriever(
+    embeddings=embeddings, 
+    sparse_encoder=bm25_encoder, 
+    index=index
 )
+retriever.add_documents(text_chunks)
 
